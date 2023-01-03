a48a6e4504a5cd9d3bc5adc02cd582eaf7e66dac
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-01-03 21:47:22 +0200

    Add navigation script

---
+++ js/navigation.js
@@ -0,0 +1,26 @@
+"use strict"
+
+window.addEventListener('load', windowLoad);
+
+function windowLoad() {
+    document.addEventListener('click', documentActions);
+}
+
+function documentActions(e) {
+    const targetElement = e.target;
+
+    // Scroll to...
+    if (targetElement.hasAttribute('data-goto')) {
+        const gotoElement = document.querySelector(`${targetElement.dataset.goto}`);
+        const headerHeight = document.querySelector(`.header`).offsetHeight;
+
+        if (gotoElement) {
+            window.scrollTo({
+                top: gotoElement.offsetTop - headerHeight,
+                behavior: "smooth"
+            })
+        }
+
+        e.preventDefault();
+    }
+}
